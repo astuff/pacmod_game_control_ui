836e8fb271b8ef1bd3dd3b0fdf175c5938a27771
Calib Fenoglio <cfenoglio@autonomoustuff.com>
2019-07-31 16:04:14 -0500

    code review changes removed comments, added linter

--- src/game_controller_ui.py
+++ src/game_controller_ui.py
@@ -1,44 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 
-# Created by: PyQt4 UI code generator 4.11.4
-
+#STD lib
 import os
-#import sys
+import sys
 import time
 
-from PyQt4 import QtCore, QtGui
-from PyQt4.QtCore import SIGNAL, SLOT
-from PyQt4.QtCore import QObject
-from PyQt4.QtCore import *
-from PyQt4.QtGui import QLabel
-from PyQt4.QtGui import QApplication
+try:
+    #Qt lib
+    from PyQt4 import QtCore, QtGui
+    from PyQt4.QtCore import SIGNAL, SLOT, QObject, pyqtSignal
+    from PyQt4.QtGui import QLabel, QApplication
 
-import rospy
-from std_msgs.msg import String
-from std_msgs.msg import Float64
-from std_msgs.msg import Bool
-from pacmod_msgs.msg import *
+    #ROS lib
+    import rospy
+    import std_msgs.msg, pacmod_msgs.msg
+    from pacmod_msgs.msg import GlobalRpt, SystemRptFloat
+    from std_msgs.msg import Float64, Bool
+
+except ImportError():
+    rospy.loginfo("Module import error,/nDid you install dependencies per Github READ ME?")
 
 #Node name
-pyNode = "joy_gui"
+_pyNode = "joy_gui"
 
 #Global constant
-CONVERTER = 100
-PACMOD_RATE = 30
-PACMOD_RATE_IN_SEC = 0.33
+_CONVERTER = 100
+_PACMOD_RATE = 30
+_PACMOD_RATE_IN_SEC = 0.33
 
-# Controls for ROS MSG'S
-enable = False
-override = False
-veh_accel = 0.0
-veh_brake = 0.0
-_steer_output = 0
+#Controls for ROS MSG'S
+_enable = False
+_override = False
+_veh_accel = 0.0
+_veh_brake = 0.0
 
-# Helper variables, Controls message frequency 
-last_Enable = ''
-last_Override = ''
+#Helper variables, Controls message frequency 
+_last_Enable = ''
+_last_Override = ''
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -54,6 +53,7 @@ except AttributeError:
         return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_MainWindow(object):
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
         MainWindow.resize(454, 400)
@@ -77,17 +77,6 @@ class Ui_MainWindow(object):
         self.verticalLayout_2.setSpacing(6)
         self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
         self.pac_wheel_label = QtGui.QLabel(self.centralWidget)
-        # sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Preferred)
-        # sizePolicy.setHorizontalStretch(0)
-        # sizePolicy.setVerticalStretch(0)
-        # sizePolicy.setHeightForWidth(self.pac_wheel_label.sizePolicy().hasHeightForWidth())
-        # self.pac_wheel_label.setSizePolicy(sizePolicy)
-        # font = QtGui.QFont()
-        # font.setPointSize(15)
-        # self.pac_wheel_label.setFont(font)
-        # self.pac_wheel_label.setObjectName(_fromUtf8("pac_wheel_label"))
-        # self.verticalLayout_2.addWidget(self.pac_wheel_label, QtCore.Qt.AlignLeft)
-        # self.pac_wheel_label.setStyleSheet(_fromUtf8("color: white"))
         self.pac_wheel = QtGui.QLabel(self.centralWidget)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -95,13 +84,15 @@ class Ui_MainWindow(object):
         sizePolicy.setHeightForWidth(self.pac_wheel.sizePolicy().hasHeightForWidth())
         self.pac_wheel.setSizePolicy(sizePolicy)
         self.pac_wheel.setText(_fromUtf8(""))
-        self.pac_wheel.setPixmap(QtGui.QPixmap(_fromUtf8("../../../../pacmod_game_control_ui/src/pacmod_game_control_ui/autonomy_images/overridewheel(80).png")))
+        self.pac_wheel.setPixmap(QtGui.QPixmap(_fromUtf8("/home/calib_fenoglio/pacmod_game_control_ui/src/pacmod_game_control_ui/autonomy_images/overridewheel(80).png")))
         self.pac_wheel.setObjectName(_fromUtf8("pac_wheel"))
         self.verticalLayout_2.addWidget(self.pac_wheel, QtCore.Qt.AlignLeft)
         self.horizontalLayout_2.addLayout(self.verticalLayout_2)
         self.pacmod_label = QtGui.QLabel(self.centralWidget)
         font = QtGui.QFont()
         font.setPointSize(15)
+        font.setBold(True)
+        font.setWeight(75)
         self.pacmod_label.setFont(font)
         self.pacmod_label.setStyleSheet(_fromUtf8("background-color: green"))
         self.pacmod_label.setObjectName(_fromUtf8("pacmod_label"))
@@ -111,7 +102,6 @@ class Ui_MainWindow(object):
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        #sizePolicy.setHeightForWidth(self.pacmod_label.sizePolicy().hasHeightForWidth())
         self.pac_wheel.setSizePolicy(sizePolicy)
         self.horizontalLayout_2.addWidget(self.pacmod_label)
         self.verticalLayout.addLayout(self.horizontalLayout_2)
@@ -131,17 +121,18 @@ class Ui_MainWindow(object):
         font.setWeight(75)
         self.acceleration_label.setFont(font)
         self.acceleration_label.setObjectName(_fromUtf8("acceleration_label"))
-        self.acceleration_label.setStyleSheet(_fromUtf8("color: white"))
+        self.acceleration_label.setStyleSheet(_fromUtf8(" white;color: white"))
         self.acceleration_label.setAlignment(QtCore.Qt.AlignHCenter)
         self.verticalLayout_3.addWidget(self.acceleration_label, QtCore.Qt.AlignHCenter)
         self.acceleration_bar = QtGui.QProgressBar(self.centralWidget)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.MinimumExpanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.acceleration_bar.sizePolicy().hasHeightForWidth())
         self.acceleration_bar.setSizePolicy(sizePolicy)
         self.acceleration_bar.setProperty("value", 0)
         self.acceleration_bar.setObjectName(_fromUtf8("acceleration_bar"))
+        self.acceleration_bar.setStyleSheet(_fromUtf8("color: white"))
         self.verticalLayout_3.addWidget(self.acceleration_bar)
         self.braking_label = QtGui.QLabel(self.centralWidget)
         font = QtGui.QFont()
@@ -161,6 +152,7 @@ class Ui_MainWindow(object):
         self.braking_bar.setSizePolicy(sizePolicy)
         self.braking_bar.setProperty("value", 0)
         self.braking_bar.setObjectName(_fromUtf8("braking_bar"))
+        self.braking_bar.setStyleSheet(_fromUtf8("color: white"))
         self.verticalLayout_3.addWidget(self.braking_bar)
         self.verticalLayout.addLayout(self.verticalLayout_3)
         self.horizontalLayout = QtGui.QHBoxLayout()
@@ -171,17 +163,15 @@ class Ui_MainWindow(object):
         self.horizontalLayout.addItem(spacerItem)
         self.as_logo = QtGui.QLabel(self.centralWidget)
         self.as_logo.setText(_fromUtf8(""))
-        self.as_logo.setPixmap(QtGui.QPixmap(_fromUtf8("../../../../pacmod_game_control_ui/src/pacmod_game_control_ui/autonomy_images/as_no_bg(80).png")))
+        self.as_logo.setPixmap(QtGui.QPixmap(_fromUtf8("/home/calib_fenoglio/pacmod_game_control_ui/src/pacmod_game_control_ui/autonomy_images/as_no_bg(80).png")))
         self.as_logo.setAlignment(QtCore.Qt.AlignRight)
         self.as_logo.setObjectName(_fromUtf8("as_logo"))
         self.horizontalLayout.addWidget(self.as_logo, QtCore.Qt.AlignRight|QtCore.Qt.AlignVCenter)
         self.verticalLayout.addLayout(self.horizontalLayout)
         MainWindow.setCentralWidget(self.centralWidget)
- 
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
 
         self.statusBar = QtGui.QStatusBar(MainWindow)
         self.statusBar.setObjectName(_fromUtf8("statusBar"))
@@ -192,7 +182,6 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "PACMod game control", None))
-        #self.pac_wheel_label.setText(_translate("MainWindow", "PACMod Indicator", None))
         self.pacmod_label.setText(_translate("MainWindow", "Ready", None))
         self.acceleration_label.setText(_translate("MainWindow", "Acceleration", None))
         self.braking_label.setText(_translate("MainWindow", "Braking", None))
@@ -204,40 +193,39 @@ class JoyGui(object):
         Checks if Pacmod is enabled and returns status
         """
 
-        #Globals for GUI
-        global last_Enable
-        global enable
+        global _last_Enable
+        global _enable
 
         # Checks for repetitive messages
-        if msg.data != last_Enable:
-            last_Enable = msg.data
+        if msg.data != _last_Enable:
+            _last_Enable = msg.data
             rospy.loginfo("NEW msg: PACMod Enabled = %s", msg.data)
-            enable = msg.data # Set message for processing
+            _enable = msg.data # Set message for processing
 
 
     def override_Check_CB(self,msg):
         """
-        Checks if driver has taken back control of vehicle
+        Checks if driver has overridden game controller
         """
-        # Globals for GUI
-        global last_Override
-        global override
+
+        global _last_Override
+        global _override
 
         # Checks for repetitive messages
-        if msg.override_active != last_Override:
-            last_Override = msg.override_active
+        if msg.override_active != _last_Override:
+            _last_Override = msg.override_active
             rospy.loginfo("NEW msg: Override = %s", msg.override_active)
-            override = msg.override_active # Set message for proccessing
+            _override = msg.override_active # Set message for proccessing
 
     def accel_Percent_CB(self, msg):
         """
         Retrieves throttle position as a float number signifying percent:
         fully closed = 0.0 and fully open = 1.0
         """
-        global veh_accel
+        global _veh_accel
 
-        if enable == True:
-            veh_accel = int(msg.output * CONVERTER)
+        if _enable == True:
+            _veh_accel = int(msg.output * _CONVERTER)
             #rospy.loginfo(rospy.get_name() + " Acceleration output: %f", (veh_accel)) # Sends info to log
 
     def brake_Percent_CB(self,msg):
@@ -245,32 +233,11 @@ class JoyGui(object):
         Retrieves braking position as a float number signifying percent:
         no braking = 0.0 and fully braking = 1.0
         """
-        global veh_brake
+        global _veh_brake
         
-        if enable == True:
-            veh_brake = int(msg.output * CONVERTER)
+        if _enable == True:
+            _veh_brake = int(msg.output * _CONVERTER)
             #rospy.loginfo(rospy.get_name() + " Braking output: %f", (veh_brake)) # Sends info to log
-
-    def steer_Converter_CB(self,msg):
-        global _steer_output
-
-        if enable == True:
-            _steer_output = msg.output
-            print (_steer_output * (180/3.14))
-
-    # def overArray(self,msg):
-    #     pass
-    #     # [1] = brake system on lexus and truck
-    #     # [3] = steering on truck
-    #     # [5] = steering on lexus
-
-    #     print "Now in over ride checker"
-
-    #     print "Brake = " + msg.overridden_status[1].value
-
-    #     print "Steer = " + msg.overridden_status[5].value
-
-    #     #print msg.overridden_status[5].value
 
     def subscribe(self):
 
@@ -278,15 +245,13 @@ class JoyGui(object):
         rospy.loginfo("Ready to publish... \n")
 
         # Set to match Pacmod rate
-        self.rate = rospy.Rate(PACMOD_RATE) # 30 HZ
+        self.rate = rospy.Rate(_PACMOD_RATE) # 30 HZ
 
         # #ROS Subscriber values 
         self.systemSub = rospy.Subscriber('/pacmod/as_tx/enabled',std_msgs.msg.Bool,self.enabled_Check_CB, queue_size= 100)
         self.sysOverRideSub = rospy.Subscriber('/pacmod/parsed_tx/global_rpt',pacmod_msgs.msg.GlobalRpt,self.override_Check_CB, queue_size=100)
         self.throttleSub = rospy.Subscriber('/pacmod/parsed_tx/accel_rpt',pacmod_msgs.msg.SystemRptFloat,self.accel_Percent_CB,queue_size= 100)
         self.brakeSub = rospy.Subscriber('/pacmod/parsed_tx/brake_rpt',pacmod_msgs.msg.SystemRptFloat,self.brake_Percent_CB, queue_size= 100)
-        self.steerSub = rospy.Subscriber('/pacmod/parsed_tx/steer_rpt',pacmod_msgs.msg.SystemRptFloat,self.steer_Converter_CB, queue_size= 100)
-        #self.allSysOver = rospy.Subscriber('/pacmod/as_tx/all_system_statuses',pacmod_msgs.msg.AllSystemStatuses, self.overArray, queue_size=100)
 
 class MyThread(QtCore.QThread):
 
@@ -306,42 +271,35 @@ class MyThread(QtCore.QThread):
     def run(self):
 
         # Overridden run method handles all main running logic
-        try:
             while not rospy.is_shutdown():
 
-                while (enable == True) and (override == False):
+                while (_enable == True) and (_override == False):
+                    self.enable_signal.emit(_enable)
+                    time.sleep(_PACMOD_RATE_IN_SEC)
+                    self.accel_signal.emit(_veh_accel)
+                    self.brake_signal.emit(_veh_brake)
 
-                    self.enable_signal.emit(enable)
-                    time.sleep(PACMOD_RATE_IN_SEC)
-
-                    self.accel_signal.emit(veh_accel)
-                    #time.sleep(PACMOD_RATE_IN_SEC)
-
-                    self.brake_signal.emit(veh_brake)
-                    #time.sleep(PACMOD_RATE_IN_SEC)
-
-                if (enable == False) and (override == False):
-                    self.enable_signal.emit(enable)
-                    time.sleep(PACMOD_RATE_IN_SEC)
+                if (_enable == False) and (_override == False):
+                    self.enable_signal.emit(_enable)
+                    time.sleep(_PACMOD_RATE_IN_SEC)
                     self.accel_signal.emit(0)
-                    time.sleep(PACMOD_RATE_IN_SEC)
+                    time.sleep(_PACMOD_RATE_IN_SEC)
                     self.brake_signal.emit(0)
-                    time.sleep(PACMOD_RATE_IN_SEC)
+                    time.sleep(_PACMOD_RATE_IN_SEC)
 
     
-                elif (override == True) and (enable == False):
-                    self.enable_signal.emit(enable)
-                    self.override_signal.emit(override)
-                    time.sleep(PACMOD_RATE_IN_SEC)
+                elif (_override == True) and (_enable == False):
+                    self.enable_signal.emit(_enable)
+                    self.override_signal.emit(_override)
+                    time.sleep(_PACMOD_RATE_IN_SEC)
                     self.accel_signal.emit(0)
-                    time.sleep(PACMOD_RATE_IN_SEC)
+                    time.sleep(_PACMOD_RATE_IN_SEC)
                     self.brake_signal.emit(0)
-                    time.sleep(PACMOD_RATE_IN_SEC)
+                    time.sleep(_PACMOD_RATE_IN_SEC)
 
-                elif (override == True) and (enable == True):
+                elif (_override == True) and (_enable == True):
                     print "Something went wrong..."
-        except:
-            pass
+
 
 class MyApp(QtGui.QMainWindow, Ui_MainWindow):
 
@@ -354,7 +312,7 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
 
         #Initialize/show UI
         self.setupUi(self)
-        self.setStyleSheet("background-color: black");
+        self.setStyleSheet("background-color: black")
         self.show()
 
         # Init thread and connect signals to slots to do work
@@ -378,7 +336,6 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         
     @QtCore.pyqtSlot(bool)
     def set_enable(self, data):
-
         """
         if Pacmod enabled, Sets indicator to blue, 
         push's status and changes background color
@@ -387,18 +344,17 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         if data == True:
             self.pacmod_label.setStyleSheet(_fromUtf8("background-color: rgb(98, 177, 246); color: white"))
             self.pacmod_label.setText("Enabled")
-            self.pac_wheel.setPixmap(QtGui.QPixmap(_fromUtf8("../../../../pacmod_game_control_ui/src/pacmod_game_control_ui/autonomy_images/autonomouswheel(80).png")))
+            self.pac_wheel.setPixmap(QtGui.QPixmap(_fromUtf8("/home/calib_fenoglio/pacmod_game_control_ui/src/pacmod_game_control_ui/autonomy_images/autonomouswheel(80).png")))
 
-        elif data == False and (override == False):
+        elif data == False and (_override == False):
             self.pacmod_label.setStyleSheet(_fromUtf8("background-color: green; color: white"))
             self.pacmod_label.setText("Ready")
-            self.pac_wheel.setPixmap(QtGui.QPixmap(_fromUtf8("../../../../pacmod_game_control_ui/src/pacmod_game_control_ui/autonomy_images/overridewheel(80).png")))
+            self.pac_wheel.setPixmap(QtGui.QPixmap(_fromUtf8("/home/calib_fenoglio/pacmod_game_control_ui/src/pacmod_game_control_ui/autonomy_images/overridewheel(80).png")))
         self.update()
 
 
     @QtCore.pyqtSlot(bool)
-    def set_override(self):
-
+    def set_override(self,data):
         """
         If override is active, indicators are set to green, 
         push's status and changes background color
@@ -406,36 +362,38 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
 
         self.pacmod_label.setStyleSheet(_fromUtf8("background-color: green; color: white"))
         self.pacmod_label.setText("Over-Ride")
-        self.pac_wheel.setPixmap(QtGui.QPixmap(_fromUtf8("../../../../pacmod_game_control_ui/src/pacmod_game_control_ui/autonomy_images/overridewheel(80).png")))
+        self.pac_wheel.setPixmap(QtGui.QPixmap(_fromUtf8("/home/calib_fenoglio/pacmod_game_control_ui/src/pacmod_game_control_ui/autonomy_images/overridewheel(80).png")))
         self.update()
 
 
     @QtCore.pyqtSlot(int)
     def set_accel_bar(self,data):
-        
         """
-        Sets acceleration percentage when pacmod game control is active
+        Sets acceleration percentage when pacmod game control is enabled
         """
 
         self.acceleration_bar.setProperty("value", data)
 
     @QtCore.pyqtSlot(int)
     def set_brake_bar(self,data):
-
         """
-        Sets braking percentage when pacmod game control is active
+        Sets braking percentage when pacmod game control is enabled
         """
 
         self.braking_bar.setProperty("value", data)
 
 
 if __name__ == "__main__":
-    
-    # init Qnode here 
-    rospy.init_node(pyNode)
-    joy_gui = JoyGui()
-    joy_gui.subscribe()
-    
+
+    try:
+        # init Qnode here 
+        rospy.init_node(_pyNode)
+        joy_gui = JoyGui()
+        joy_gui.subscribe()
+
+    except rospy.ROSInterruptException:
+        rospy.logwarn("joy gui Error")
+
     #Init application window
     app = QtGui.QApplication(sys.argv)
     app.setStyle('cleanlooks')
